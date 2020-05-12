a6e40017c6ee17c5235ce2b37fbe58c407aa82bd
Ryan Bahan <ryan@ryanbahan.com>
2020-05-12 15:01:33 -0600

    Updates

---
+++ precache-manifest.4c6779543dd99c3a39a158723024bb9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21d1581189cd73bee6241f944196ffbd",
+    "url": "/broadnet-todo/index.html"
+  },
+  {
+    "revision": "b151535f0f0b6ffe6992",
+    "url": "/broadnet-todo/static/css/main.cc3cfafc.chunk.css"
+  },
+  {
+    "revision": "09c814fbed5248fc086f",
+    "url": "/broadnet-todo/static/js/2.62053479.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/broadnet-todo/static/js/2.62053479.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b151535f0f0b6ffe6992",
+    "url": "/broadnet-todo/static/js/main.bf03ed0a.chunk.js"
+  },
+  {
+    "revision": "91282239487d3f7ef613",
+    "url": "/broadnet-todo/static/js/runtime-main.5e5572dd.js"
+  }
+]);
