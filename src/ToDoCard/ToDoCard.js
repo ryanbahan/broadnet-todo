759b40f18268e0e722b27acba2806cf8618655c6
Ryan Bahan <ryan@ryanbahan.com>
2020-05-12 11:23:56 -0600

    Create static cards

--- src/ToDoCard/ToDoCard.js
+++ src/ToDoCard/ToDoCard.js
@@ -4,7 +4,19 @@ import './ToDoCard.css';
 function ToDoCard() {
   return (
     <article className="todo-card">
-      post
+      <div className="card-top">
+        <p>Task Title</p>
+      </div>
+      <div className="card-list-items">
+        <ul>
+          <li><input type="checkbox" /> item a</li>
+          <li><input type="checkbox" /> item b</li>
+        </ul>
+      </div>
+      <div className="card-bottom">
+        <button>Urgent</button>
+        <button>Delete</button>
+      </div>
     </article>
   );
 }
